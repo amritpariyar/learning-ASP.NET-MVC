1faaf5e0dd2a93cb7cb7218ed346f2ef2c31bef8
amritpariyar <amrit.pariyar1@gmail.com>
2019-11-25 21:35:46 +0545

    Learning API

---
+++ SOIT/Controllers/ExchangeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace SOIT.Controllers
+{
+    public class ExchangeController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetOrganizationInformation()
+        {
+            var detail = new
+            {
+                Name = "Civil",
+                Address = "Kathmandu"
+            };
+
+            return Request.CreateResponse(HttpStatusCode.OK, detail, new JsonMediaTypeFormatter(), "application/json");
+        }
+    }
+}
