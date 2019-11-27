9df6eadd6974057980240e5a3e456e04cbebebd4
amritpariyar <amrit.pariyar1@gmail.com>
2019-11-27 16:17:55 +0545

    string manupulation

--- SOIT/Controllers/ExchangeController.cs
+++ SOIT/Controllers/ExchangeController.cs
@@ -28,7 +28,33 @@ namespace SOIT.Controllers
                 return Request.CreateResponse(HttpStatusCode.Conflict, "Data Conflict occured", new JsonMediaTypeFormatter(), "application/json");
                 //throw;
             }
+
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetOutputResult()
+        {
+            string output1 = @"AUD1000000010100//1,
+                    CATEGORY=10000:1:1,
+                    ACCOUNT.TITLE.1=AUSTRALIAN DOLLER:1:1,
+                    ACCOUNT.TITLE.2=AUSTRALIAN DOLLER:1:1,
+                    SHORT.TITLE=AUSTRALIAN DOLLER:1:1,
+                    MNEMONIC=JEEVANTEST:1:1,
+                    POSITION.TYPE=TR:1:1,
+                    CURRENCY=AUD:";
+
+            List<string> splittedOutput = output1.Split(',').ToList();
+            string[] accno = splittedOutput.First().Replace("//","_").Split('_');
+            string AccountNo = accno[1]=="1" ? accno[0]:"";
+            string AccountTitleObj = splittedOutput.Where(a => a.Contains("TITLE")).FirstOrDefault();
+            string AccountName = AccountTitleObj.Split('=')[1];
             
+            var result = new
+            {
+                AccountNo = AccountNo,
+                AccountName=AccountName,
+            };
+            return Request.CreateResponse(HttpStatusCode.OK, result, new JsonMediaTypeFormatter(), "application/json");
         }
     }
 }
