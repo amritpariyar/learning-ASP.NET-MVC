5ed43911ee625b2f47d8facb18a9e5907d839e0f
amritpariyar <amrit.pariyar1@gmail.com>
2019-12-03 11:58:03 +0545

    User Role Management

--- SOIT/Controllers/RoleController.cs
+++ SOIT/Controllers/RoleController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 using SOIT.Data.ViewModels;
 using SOIT.Models;
 using System;
@@ -11,11 +13,24 @@ namespace SOIT.Controllers
 {
     public class RoleController : Controller
     {
+        private ApplicationUserManager _userManager;
        ApplicationDbContext context;
-        public RoleController()
+        public RoleController(ApplicationUserManager userManager)
         {
+            UserManager = userManager;
             context = new ApplicationDbContext();
             
+        }
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
         }
         /// <summary>
         /// Get All Roles
@@ -75,18 +90,42 @@ namespace SOIT.Controllers
 
         public ActionResult UserDetail(string userid)
         {
-            var usersWithRoles = (from user in context.Users
-                                  select new
-                                  {
-                                      UserId = user.Id,
-                                      Username = user.UserName,
-                                      Email = user.Email,
-                                      RoleNames = (from userRole in user.Roles
-                                                   join role in context.Roles on userRole.RoleId
-                                                   equals role.Id
-                                                   select role.Name).ToList()
-                                  }).ToList();
+            var roles = context.Roles.ToList().Select(r=>r.Name).ToList();
+            ViewBag.roles = roles;
+            var userList = context.Users.OrderBy(u => u.UserName).ToList().Select(ur => new SelectListItem { Value = ur.Id.ToString(), Text = ur.UserName }).ToList();
+            ViewBag.Users = userList;
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetRolesWithUser(string Id)
+        {
+            var roleList = context.Roles.OrderBy(r => r.Name).ToList()
+                .Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            var userRole = _userManager.GetRolesAsync(Id).Result.ToList();
+            return Json(userRole, JsonRequestBehavior.DenyGet);
+        }
+
+        [HttpPost]
+        public JsonResult UpdateRolesWithUser(string userid,string[] roles)
+        {
+            try
+            {
+                string[] roleList = _userManager.GetRoles(userid).ToArray<string>();
+                _userManager.RemoveFromRoles(userid, roleList);
+                if (roles.Count() > 0)
+                {
+                    _userManager.AddToRoles(userid, roles);
+                }
+                
+                return Json("success", JsonRequestBehavior.DenyGet);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
         #endregion
     }
